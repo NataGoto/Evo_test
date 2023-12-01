9b63815c4d202e8b398a74aea386cfa585cc436d
NataGoto <138012512+NataGoto@users.noreply.github.com>
2023-12-01 19:17:54 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -16,25 +16,31 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, current_directory)
 
 # Streamlit интерфейс
-st.title('Evodrone Test')
+st.title('YOLO Object Detection')
 
-# Load a pretrained YOLOv8n model
-model = YOLO('best.pt')
+# Инициализация модели YOLO
+model = YOLO('best.pt')  # Путь к файлу с весами модели
 
 # Загрузка изображения
 uploaded_image = st.file_uploader("Upload an image", type=["png", "jpg"], key='image_uploader')
 if uploaded_image is not None:
-    # Сохраняем загруженное изображение во временный файл
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_file:
-        tmp_file.write(uploaded_image.read())
-        tmp_file_path = tmp_file.name
+    # Преобразование изображения в формат, совместимый с моделью YOLO
+    image = Image.open(uploaded_image).convert('RGB')
+    image_array = np.array(image)  # Модель YOLO требует numpy array
 
-    # Выполнение предсказания
-    results = model(tmp_file_path)
+    # Обработка изображения
+    results = model(image_array)
 
-    # Получение аннотированного изображения
-    annotated_image = Image.fromarray(np.squeeze(results.render()))
-
-    # Отображение обработанного изображения
+    # Получение и отображение аннотированного изображения
+    annotated_image = results.render()[0]  # Метод render возвращает изображение с аннотациями
     st.image(annotated_image, caption='Processed Image')
+
+    # Если требуется вывести дополнительные детали предсказаний
+    for pred in results.pred[0]:
+        # pred - это тензор с предсказаниями [x1, y1, x2, y2, conf, class]
+        class_id = int(pred[5])
+        class_name = model.names[class_id]
+        confidence = pred[4].item()
+        st.write(f'{class_name} {confidence:.2f}')
+
 
